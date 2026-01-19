6949626a053741b57363231d10dd85c8b4023177
thiago-miguel <thiago.alexandre@outlook.com.br>
2026-01-19 17:15:37 -0300

    Funcionamento do botão Adicionar Número

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,53 @@
-document.getElementById("calcularBtn").addEventListener("click", () => {
-    document.getElementById("resultados").classList.remove("hidden");
-});
-
+const calcularBtn = document.getElementById("calcularBtn");
+const resultadosContainer = document.getElementById("resultados");
 const numeroInput = document.getElementById("numeroInput");
+const adicionar = document.getElementById("adicionarBtn");
+const numerosLista = document.getElementById("numerosLista");
 
 const numeros = [];
+
+const adicionarNumero = () => {
+    if (numeros.length >= 20) {
+        alert("Limite de 20 números atingido!");
+        return;
+    }
+    
+    const numero = parseFloat(numeroInput.value);
+    
+    if (isNaN(numero)) {
+        alert("Por favor, insira um número válido.");
+        return;
+    }
+    
+    if (numero < -1000000 || numero > 1000000) {
+        alert("O número deve estar entre -1.000.000 e 1.000.000");
+        return;
+    }
+    
+    // Validar duas casas decimais
+    if (!/^-?\d+(\.\d{1,2})?$/.test(numeroInput.value)) {
+        alert("Aceita-se no máximo duas casas decimais.");
+        return;
+    }
+    
+    numeros.push(numero);
+    
+    // Adicionar o número à lista HTML
+    const itemLista = document.createElement("li");
+    itemLista.textContent = numero;
+    numerosLista.appendChild(itemLista);
+    
+    numeroInput.value = "";
+    numeroInput.focus();
+};
+
+adicionar.addEventListener("click", adicionarNumero);
+numeroInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        adicionarNumero();
+    }
+});
+
+calcularBtn.addEventListener("click", () => {
+    resultadosContainer.classList.remove("hidden");
+});
